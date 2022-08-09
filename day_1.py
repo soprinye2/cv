8ef904da3f1b8e7e31eb287646e1b2c0b5d3a5e1
David Wilson <dw229057@gmail.com>
2022-08-09 22:19:44 +0100

    added Exclamation

---
+++ day_1.py
@@ -0,0 +1,25 @@
+#print("Hello world!\nHello world!")
+#print("Hello " + "Cyberseckid")
+
+# print("Hello " + input("What is your name? "))
+print(len("name"))
+
+#BAND NAME GENERATOR
+print("Welcome to the Band Name Generator.")
+
+city_name = input("What's the name of the city you grew up in?\n ")
+
+pet_name = input("What's your pet's name?\n ")
+
+print("Your band name could be " + city_name + " " + pet_name) 
+
+'''
+Output
+
+Welcome to the Band Name Generator.
+What's the name of the city you grew up in?
+PH
+What's your pet's name?
+ dog
+Your band name could be PH dog
+'''
